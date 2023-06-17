f73d6511d47041369e472d3b9e98ddc4edea2669
Swarika <2021.swarnika.singh@ves.ac.in>
2023-06-17 09:43:28 +0530

    first commit

---
+++ src/Register.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react"
+export const Register = (props)  => {
+
+    const [name, setNmail] = useState ('');
+    const [email, setEmail] = useState ('');
+    const [contact, setContact] = useState ('');
+    const [address, setAddress] = useState ('');
+    const [pass, setPass] = useState ('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+        }
+
+    return (
+        <div className="auth-form-container">
+        <form className="register-form"onSubmit={handleSubmit}>
+            <h2>Register</h2>
+
+        <label htmlFor="name">Name</label>
+        <input type="name" placeholder="Full Name" id="name" name= "name"/>
+        <label htmlFor="email">User-ID</label>
+        <input type="email" placeholder="Email" id="email" name= "email"/>
+        <label htmlFor="contact ">Contact </label>
+        <input type="contact " placeholder="Contact " id="contact " name= "contact "/>
+        <label htmlFor="address">Address</label>
+        <input type="address" placeholder="Address" id="address" name= "address"/>
+        <label htmlFor="password">Password</label>
+        <input type="password" placeholder="******" id="password" name= "password"/>
+
+        <button type="Submit">Register</button>
+
+    </form>
+    <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
+    </div>
+    )
+}
