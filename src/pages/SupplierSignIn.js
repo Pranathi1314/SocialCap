c08af0fe91dbd9fc573cf67f963186499f42a9eb
Pranathi1314 <97385306+Pranathi1314@users.noreply.github.com>
2023-06-23 18:12:14 +0530

    Producer4

--- src/pages/SupplierSignIn.js
+++ src/pages/SupplierSignIn.js
@@ -1,79 +1,49 @@
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./SupplierSignIn.module.css";
+import "./SupplierSignIn.css";
 const SupplierSignIn = () => {
   const navigate = useNavigate();
 
   const onRectangle1Click = useCallback(() => {
-    // Please sync "Supplier Landing Page" to the project
-  }, []);
-
-  const onAlreadyHaveAnClick = useCallback(() => {
-    navigate("/login-page");
+    navigate("/supplier-landing-page");
   }, [navigate]);
 
   return (
-    <div className={styles.supplierSignIn}>
-      <div className={styles.consumerSignIn}>
+    <div className="supplier-sign-in">
+      <div className="consumer-sign-in">
         <img
-          className={styles.loginBackground1}
+          className="login-background-1"
           alt=""
-          src="/login-background-11@2x.png"
+          src="/login-background-1@2x.png"
         />
+        <img className="consumer-sign-in-child" alt="" src="/rectangle-8.svg" />
+        <div className="contact-no">Contact No.</div>
+        <div className="address">Address</div>
+        <div className="password">Password</div>
         <img
-          className={styles.consumerSignInChild}
-          alt=""
-          src="/rectangle-81.svg"
-        />
-        <div className={styles.contactNo}>Contact No.</div>
-        <div className={styles.address}>Address</div>
-        <div className={styles.password}>Password</div>
-        <img
-          className={styles.notoV1womanFarmerIcon}
+          className="noto-v1woman-farmer-icon"
           alt=""
           src="/notov1womanfarmer.svg"
         />
-        <b className={styles.signUp}>Sign-Up</b>
-        <div className={styles.supplierName}>Supplier Name</div>
-        <div className={styles.consumerSignInItem} />
-        <div className={styles.consumerSignInInner} />
-        <img className={styles.lineIcon} alt="" src="/line-42.svg" />
+        <b className="sign-up">Sign-Up</b>
+        <div className="supplier-name">Supplier Name</div>
+        <div className="consumer-sign-in-item" />
+        <div className="consumer-sign-in-inner" />
+        <img className="line-icon" alt="" src="/line-4.svg" />
+        <div className="line-div" />
+        <img className="consumer-sign-in-child1" alt="" src="/line-4.svg" />
+        <div className="user-id">User Id</div>
+        <div className="rectangle-div" onClick={onRectangle1Click} />
+        <div className="sign-up1">Sign Up</div>
+        <img className="riclose-line-icon" alt="" src="/ricloseline.svg" />
+        <div className="already-have-an">Already have an account? Login</div>
+        <img className="epback-icon" alt="" src="/epback.svg" />
+        <img className="riclose-line-icon1" alt="" src="/ricloseline.svg" />
+        <img className="riclose-line-icon2" alt="" src="/ricloseline.svg" />
+        <div className="riclose-line" />
+        <img className="vector-icon" alt="" src="/vector.svg" />
         <img
-          className={styles.consumerSignInChild1}
-          alt=""
-          src="/line-42.svg"
-        />
-        <img
-          className={styles.consumerSignInChild2}
-          alt=""
-          src="/line-42.svg"
-        />
-        <div className={styles.userId}>User Id</div>
-        <div className={styles.rectangleDiv} onClick={onRectangle1Click} />
-        <div className={styles.signUp1}>Sign Up</div>
-        <img
-          className={styles.ricloseLineIcon}
-          alt=""
-          src="/ricloseline3.svg"
-        />
-        <div className={styles.alreadyHaveAn} onClick={onAlreadyHaveAnClick}>
-          Already have an account? Login
-        </div>
-        <img className={styles.epbackIcon} alt="" src="/epback1.svg" />
-        <img
-          className={styles.ricloseLineIcon1}
-          alt=""
-          src="/ricloseline3.svg"
-        />
-        <img
-          className={styles.ricloseLineIcon2}
-          alt=""
-          src="/ricloseline3.svg"
-        />
-        <div className={styles.ricloseLine} />
-        <img className={styles.vectorIcon} alt="" src="/vector17.svg" />
-        <img
-          className={styles.icbaselineLockIcon}
+          className="icbaseline-lock-icon"
           alt=""
           src="/icbaselinelock.svg"
         />
